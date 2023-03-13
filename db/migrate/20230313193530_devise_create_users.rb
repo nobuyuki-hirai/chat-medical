3c037489fbd2b1aece6f0d314d3a71cb9de01308
nobuyuki-hirai <117642632+nobuyuki-hirai@users.noreply.github.com>
2023-03-14 04:42:43 +0900

    rails g devise:views create

--- db/migrate/20230313193530_devise_create_users.rb
+++ db/migrate/20230313193530_devise_create_users.rb
@@ -28,7 +28,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
 
       ## Confirmable
       # t.string   :confirmation_token
-      # t.datetime :confirmed_at
+      # t.datetime :confirmed_at 
       # t.datetime :confirmation_sent_at
       # t.string   :unconfirmed_email # Only if using reconfirmable
 

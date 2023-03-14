2c4cfda6130a0239408ab03ecaf2c4c53f58d73e
nobuyuki-hirai <117642632+nobuyuki-hirai@users.noreply.github.com>
2023-03-14 21:49:14 +0900

    models/user.rb job_id 修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,5 @@ class User < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :genre
 
-  validates :genre_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :job_id, numericality: { other_than: 1 , message: "can't be blank"}
 end

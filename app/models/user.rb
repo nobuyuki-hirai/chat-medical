cca33d9bc6d40693070082a088cdbc96c6456aac
nobuyuki-hirai <117642632+nobuyuki-hirai@users.noreply.github.com>
2023-03-16 19:13:57 +0900

    models/user.rb associations 修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,24 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :genre
+  has_many :rooms
+  has_many :user_rooms
+  has_many :messages
+  has_many :relationships
+  has_many :events
 
-  validates :job_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :last_name,             presence: true
+  validates :first_name,            presence: true
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates :password, format: { with: VALID_PASSWORD_REGEX }
+  validates :hobby,                 presence: true
+  validates :context,               presence: true
+  validates :job_id,                numericality: { other_than: 1, message: "can't be blank" }
+  
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :job, foreign_key: 'job_id'
+
+  def active_hash_model
+    ActiveHashModel.find(job_id)
+  end
 end

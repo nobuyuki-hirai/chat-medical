ed9774b6777f5bac32cc704509d4189cec26c63d
nobuyuki-hirai <117642632+nobuyuki-hirai@users.noreply.github.com>
2023-03-18 01:06:37 +0900

    rooms_controller index,new,create action修正

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,10 +1,19 @@
 class RoomsController < ApplicationController
   def index
+    @room_lists = Room.all
   end
 
   def new
+    @room = Room.new
   end
 
   def create
+    @room = Room.new(room_params)
+  end
+
+  private
+
+  def room_params
+    params.require(:room).permit(:name, user_ids: [])
   end
 end

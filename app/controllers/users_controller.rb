cf877eaf5b94e7be6fe60e60db5b98a4dc3b2379
nobuyuki-hirai <117642632+nobuyuki-hirai@users.noreply.github.com>
2023-03-17 18:30:29 +0900

    users_controller.rb showアクション修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,20 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @job = Job.find(params[:id])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:last_name, :first_name, :hobby, :context, :job_id, :image, :user_id)
+  end
 end

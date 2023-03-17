9d06f53cc4c7a2e8e6a159b330d128375c2f13c7
nobuyuki-hirai <117642632+nobuyuki-hirai@users.noreply.github.com>
2023-03-18 01:04:31 +0900

    models/room.rb association

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,2 +1,6 @@
 class Room < ApplicationRecord
+  has_many :user_rooms, dependent: :destroy
+  has_many :users, through: :user_rooms, dependent: :destroy
+
+  validates :name, presence: true, uniqueness: true
 end

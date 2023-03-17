f5938cae41a6ee7753a805f1a5fbe61ea8019e5a
nobuyuki-hirai <117642632+nobuyuki-hirai@users.noreply.github.com>
2023-03-18 01:04:55 +0900

    rooms db:migrate

--- db/migrate/20230317143025_create_rooms.rb
+++ db/migrate/20230317143025_create_rooms.rb
@@ -1,8 +1,8 @@
 class CreateRooms < ActiveRecord::Migration[6.0]
   def change
     create_table :rooms do |t|
-      t.string       :title,    null: false, default: ""
-      t.references   :user,     null: false, foreign_key: true
+      t.string       :name,    null: false
+      t.index        :name,     unique: true
       t.timestamps
     end
   end

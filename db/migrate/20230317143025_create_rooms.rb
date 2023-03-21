fa98cc9bff2124d51ec9931b87a395361b6dc9b3
nobuyuki-hirai <117642632+nobuyuki-hirai@users.noreply.github.com>
2023-03-21 23:21:40 +0900

    db room_users create

--- db/migrate/20230317143025_create_rooms.rb
+++ db/migrate/20230317143025_create_rooms.rb
@@ -1,8 +1,8 @@
 class CreateRooms < ActiveRecord::Migration[6.0]
   def change
     create_table :rooms do |t|
-      t.string       :name,    null: false
-      t.index        :name,     unique: true
+      t.string       :name,      null: false
+      t.index        :name,      unique: true
       t.timestamps
     end
   end

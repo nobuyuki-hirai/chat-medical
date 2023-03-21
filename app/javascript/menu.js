15cf64865922615e6477b4f456f74e6da9bb1f06
nobuyuki-hirai <117642632+nobuyuki-hirai@users.noreply.github.com>
2023-03-21 14:59:27 +0900

    javascript/menu.js 修正

--- app/javascript/menu.js
+++ app/javascript/menu.js
@@ -1,55 +1,26 @@
-var selectedUsers = [];
-var selectBox = document.querySelector('select');
-var userList = document.querySelector('.selected-user-list');
+window.addEventListener('load', function(){
+  const selectList = document.querySelector('.invitation-user__list');
+  const selectedList = document.querySelector('.selected-user__list');
+  const selectedOptions = new Set();
 
-selectBox.addEventListener('change', function() {
+  selectList.addEventListener('change', () => {
+    const selectedValue = selectList.value;
 
-  var selectedOption = selectBox.options[selectBox.selectedIndex];
-  var selectedUserId = selectedOption.value;
-
-  if (selectedUsers.indexOf(selectedUserId) !== -1) {
-    return;
-  }
-
-  selectedUsers.push(selectedUserId);
-
-  var userElement = document.createElement('div');
-  userElement.classList.add('selected-user');
-  userElement.dataset.userId = selectedUserId;
-  userElement.textContent = selectedOption.textContent;
-
-  var removeButton = document.createElement('span');
-  removeButton.classList.add('remove-user');
-  removeButton.textContent = '×';
-  removeButton.addEventListener('click', function(event) {
-    var userElement = event.target.parentNode;
-    var userId = userElement.dataset.userId;
-
-    userElement.style.display = 'none';
-
-    selectedUsers = selectedUsers.filter(function(id) {
-      return id !== userId;
-    });
-  });
-  userElement.appendChild(removeButton);
-
-  userList.appendChild(userElement);
-
-  var hiddenUsers = [];
-
-  selectBox.addEventListener('change', function() {
-
-    var restoredElement = hiddenUsers.find(function(element) {
-      return element.dataset.userId === selectedUserId;
-    });
-
-    if (restoredElement) {
-
-      restoredElement.style.display = 'block';
-
-      hiddenUsers = hiddenUsers.filter(function(element) {
-        return element.dataset.userId !== selectedUserId;
-      });
+    if (selectedOptions.has(selectedValue)) {
+      alert('既に選択されています');
+      return;
     }
+
+    const selectedOption = document.createElement('div');
+    selectedOption.innerText = selectedValue;
+    selectedList.appendChild(selectedOption);
+
+    selectedOption.addEventListener('click', () => {
+      selectedOptions.delete(selectedValue);
+      selectedOption.remove();
+    });
+
+    selectedOptions.add(selectedValue);
+    selectList.selectedIndex = 0;
   });
 });

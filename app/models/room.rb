5ab6775e5b179e8dceac3bc3eada71a32578615b
nobuyuki-hirai <117642632+nobuyuki-hirai@users.noreply.github.com>
2023-03-21 14:55:32 +0900

    model/room.rb association 修正

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,6 +1,12 @@
 class Room < ApplicationRecord
   has_many :user_rooms, dependent: :destroy
   has_many :users, through: :user_rooms, dependent: :destroy
+  has_many :messages
+  has_one  :events
 
-  validates :name, presence: true, uniqueness: true
+  mount_uploader :image, ImageUploader
+
+  validates :name,    presence: true, uniqueness: true
+  validates :image, presence: true
+
 end

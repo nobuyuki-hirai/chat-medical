845dafac3da0292cbffea37da66f4c289eb3f85c
nobuyuki-hirai <117642632+nobuyuki-hirai@users.noreply.github.com>
2023-03-21 23:23:46 +0900

    models/room.rb create

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,11 +1,10 @@
 class Room < ApplicationRecord
-  has_many :user_rooms, dependent: :destroy
-  has_many :users, through: :user_rooms, dependent: :destroy
+  has_many :room_users, dependent: :destroy
+  has_many :users, through: :room_users
   has_many :messages
   has_one  :events
 
-  mount_uploader :image, ImageUploader
-
   validates :name, presence: true, uniqueness: true
-  validates :image, presence: true
+
+  mount_uploader :image, ImageUploader
 end

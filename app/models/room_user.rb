99feac9893e5d99f36cf68c1a84d7b725ab74b2d
nobuyuki-hirai <117642632+nobuyuki-hirai@users.noreply.github.com>
2023-03-24 05:35:20 +0900

    models/room_user.rb 修正

--- app/models/room_user.rb
+++ app/models/room_user.rb
@@ -1,6 +1,4 @@
 class RoomUser < ApplicationRecord
-  belongs_to :room
-  belongs_to :user
-
-  validates :user_id, presence: true
+  belongs_to :room, optional: true
+  belongs_to :user, optional: true
 end

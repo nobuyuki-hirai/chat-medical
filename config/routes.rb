6649c8ea8e3bf56a3aa56f39e7b81af33a59269c
nobuyuki-hirai <117642632+nobuyuki-hirai@users.noreply.github.com>
2023-03-26 00:51:38 +0900

    routes.rb 修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "rooms#index"
-  resources :users, only: :show
+  resources :users, only: [:show, :edit, :update]
   resources :rooms
 
   get 'rooms/revert/:id', to: 'rooms#revert_check'

8df1d8a7c28bd7db652d6cd437d81b7d0c1b7833
nobuyuki-hirai <117642632+nobuyuki-hirai@users.noreply.github.com>
2023-03-26 16:15:49 +0900

    routes.rb get search create

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "rooms#index"
   resources :users, only: [:show, :edit, :update]
-  resources :rooms
+  resources :rooms do
+    collection do
+      get 'search'
+    end
+  end
 
   get 'rooms/revert/:id', to: 'rooms#revert_check'
 end

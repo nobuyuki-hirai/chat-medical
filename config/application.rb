8246ccdffdb27f1ea61423e6177bcc7e16eb5740
nobuyuki-hirai <117642632+nobuyuki-hirai@users.noreply.github.com>
2023-03-27 21:01:35 +0900

    date.ja 表示

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,8 @@ module ChatMedical
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-
+    config.i18n.default_locale = :ja
+    config.time_zone = 'Tokyo'
     config.assets.enabled = true
 
     # Settings in config/environments/* take precedence over those specified here.

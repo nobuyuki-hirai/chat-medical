e2c738f4d7fa80a576035a3ad778eddc9a6357a5
nobuyuki-hirai <117642632+nobuyuki-hirai@users.noreply.github.com>
2023-03-28 05:16:23 +0900

    models/messages validates create

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,7 +2,8 @@ class Message < ApplicationRecord
   belongs_to :room
   belongs_to :user
 
-  validates :content, presence: true
+  validates :content, presence: true, unless: -> { image.present? }
+  validates :image, presence: true, unless: -> { content.present? }
 
   mount_uploader :image, ImageUploader
 end

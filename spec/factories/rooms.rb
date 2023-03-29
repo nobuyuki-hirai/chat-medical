6c274b84eed1f6e10fc9ae461908e2f9b3fff854
nobuyuki-hirai <117642632+nobuyuki-hirai@users.noreply.github.com>
2023-03-29 22:50:02 +0900

    factories/rooms.rb create

--- spec/factories/rooms.rb
+++ spec/factories/rooms.rb
@@ -1,4 +1,9 @@
 FactoryBot.define do
   factory :room do
+    name            { 'サンプルルーム' }
+    creator         { 'create(:user)' }
+    image           { Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/fixtures/test.jpg')) }
+
+    association :user
   end
 end

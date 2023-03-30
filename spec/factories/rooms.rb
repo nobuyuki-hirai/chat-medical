ede1849c0089a455a24925ff36fddc2861e3015a
nobuyuki-hirai <117642632+nobuyuki-hirai@users.noreply.github.com>
2023-03-31 01:51:10 +0900

    factories/rooms.rb 修正

--- spec/factories/rooms.rb
+++ spec/factories/rooms.rb
@@ -1,9 +1,12 @@
 FactoryBot.define do
   factory :room do
     name            { 'サンプルルーム' }
-    creator         { 'create(:user)' }
+    creator         { association :user }
     image           { Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/fixtures/test.jpg')) }
+    user_ids        { [association(:user).id, association(:user).id, association(:user).id] }
 
-    association :user
+    after(:build) do | room, evaluator |
+      room.user_ids = evaluator.user_ids
+    end
   end
 end

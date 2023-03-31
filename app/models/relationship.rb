ab0a3b5156f4be2d13ab2e9c17a9bcf7f8d11c71
nobuyuki-hirai <117642632+nobuyuki-hirai@users.noreply.github.com>
2023-04-01 00:53:20 +0900

    models/relationships.rb 修正

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,4 +1,9 @@
 class Relationship < ApplicationRecord
   belongs_to :following, class_name: 'User'
   belongs_to :follower, class_name: 'User'
+
+  def followed_by?(user)
+    follower = passive_relationships.find_by(following_id: user.id)
+    follower.present?
+  end
 end

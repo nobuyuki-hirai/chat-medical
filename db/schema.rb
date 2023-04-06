858f48525cb725fd03136d8c39a5bc952b05c395
nobuyuki-hirai <117642632+nobuyuki-hirai@users.noreply.github.com>
2023-04-07 03:42:04 +0900

    event db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_31_103755) do
+ActiveRecord::Schema.define(version: 2023_04_06_080507) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -31,6 +31,20 @@ ActiveRecord::Schema.define(version: 2023_03_31_103755) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "events", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.string "text"
+    t.string "place"
+    t.datetime "date_and_time"
+    t.datetime "event_finish_time"
+    t.bigint "true_id"
+    t.bigint "false_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["false_id"], name: "index_events_on_false_id"
+    t.index ["true_id"], name: "index_events_on_true_id"
   end
 
   create_table "messages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|

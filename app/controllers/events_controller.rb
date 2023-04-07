1e52a81f790747a7ba1a6d0cb929859e845fafca
nobuyuki-hirai <117642632+nobuyuki-hirai@users.noreply.github.com>
2023-04-08 04:28:24 +0900

    events_controller.rb. 修正

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,4 +2,24 @@ class EventsController < ApplicationController
   def index
     @events = Event.all
   end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @room = Room.find(params[:room_id])
+    @event = @room.events.new(event_params)
+    if @event.save
+      redirect_to room_events_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:name, :text, :place, :start_time, :end_time).merge(user_id: current_user.id)
+  end
 end

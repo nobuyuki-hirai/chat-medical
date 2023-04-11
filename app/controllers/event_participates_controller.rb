1308364a54db33394e0bbd0f3b6a958921e9c3fc
nobuyuki-hirai <117642632+nobuyuki-hirai@users.noreply.github.com>
2023-04-11 09:31:42 +0900

    rubocop -a 修正

--- app/controllers/event_participates_controller.rb
+++ app/controllers/event_participates_controller.rb
@@ -4,9 +4,9 @@ class EventParticipatesController < ApplicationController
   def create
     @event_participate = EventParticipate.new(event_id: params[:event_id], user_id: current_user.id)
     if @event_participate.save
-      flash[:success] = "参加しました。"
+      flash[:success] = '参加しました。'
     else
-      flash[:danger] = "参加に失敗しました。"
+      flash[:danger] = '参加に失敗しました。'
     end
     redirect_to room_events_path
   end
@@ -14,11 +14,10 @@ class EventParticipatesController < ApplicationController
   def destroy
     @event_participate = EventParticipate.find_by(event_id: params[:event_id], user_id: current_user.id)
     if @event_participate.destroy
-      flash[:success] = "参加を取り消しました。"
+      flash[:success] = '参加を取り消しました。'
     else
-      flash[:danger] = "参加の取り消しに失敗しました。"
+      flash[:danger] = '参加の取り消しに失敗しました。'
     end
     redirect_to room_events_path
   end
 end
-

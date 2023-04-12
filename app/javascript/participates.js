35ca19745ce2c8533274cc259253e5264a2436c2
nobuyuki-hirai <117642632+nobuyuki-hirai@users.noreply.github.com>
2023-04-12 21:37:06 +0900

    js/participates.js 修正

--- app/javascript/participates.js
+++ app/javascript/participates.js
@@ -1,10 +1,14 @@
 function pullDown() {
+  const pullDownButton = document.getElementById("participant-dropdown");
+  const pullDownParents = document.getElementById("pull-down");
 
-  const pullDownButton = document.getElementById("participant-dropdown")
-  const pullDownParents = document.getElementById("pull-down")
-
-  pullDownButton.addEventListener('click', function() {
+  pullDownButton.addEventListener('mouseover', function() {
     pullDownParents.classList.toggle("hidden");
-  })
+  });
+
+  pullDownParents.addEventListener('mouseout', function() {
+    pullDownParents.classList.add("hidden");
+  });
 }
-window.addEventListener('load', pullDown)
+
+window.addEventListener('load', pullDown);

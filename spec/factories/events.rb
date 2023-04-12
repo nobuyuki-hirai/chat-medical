f16702f49ad20f94bc81edc8960d5c4850ddd9a0
nobuyuki-hirai <117642632+nobuyuki-hirai@users.noreply.github.com>
2023-04-12 23:02:03 +0900

    factories/events.rb create

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -1,4 +1,13 @@
 FactoryBot.define do
   factory :event do
+    name            { 'テストイベント' }
+    text            { '宜しくお願いします。' }
+    place           { '自宅' }
+    start_time      { Time.current + 1.hour }
+    end_time        { Time.current + 2.hour }
+
+    association :user
+    association :room
+    association :organizer, factory: :user
   end
 end

23c2c781325ca04d37f5923276ac6d8c773c9eb6
nobuyuki-hirai <117642632+nobuyuki-hirai@users.noreply.github.com>
2023-04-18 13:15:14 +0900

    models/talk.rb 修正

--- app/models/talk.rb
+++ app/models/talk.rb
@@ -1,6 +1,16 @@
 class Talk < ApplicationRecord
   belongs_to :sender, class_name: "User"
-  belongs_to :recipient, class_name: "User"
+  has_many :reads,    dependent: :destroy
+
+  attribute :read, :boolean, default: false
+
+  validates :text, presence: true, unless: -> { image.present? }
+  validates :image, presence: true, unless: -> { text.present? }
 
   mount_uploader :image, ImageUploader
+
+  def mark_as_read!
+    self.read = true
+    save
+  end
 end
